d464c8608e041ad71c068157e07433db499fa636
Pjaerr <joshcharlesjackson@gmail.com>
2020-05-19 15:42:53 +0100

    Laid out the tabs component a bit
    Will need to properly style this. Also added very basic refresh functionality that doesn't actually refresh anything

--- app/components/collection/tabs.js
+++ app/components/collection/tabs.js
@@ -2,20 +2,8 @@ import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 
-export default class CollectionComponent extends Component {
-  @tracked activeCollection = this.args.collections[0];
+export default class CollectionTabsComponent extends Component {
   @tracked isRefreshBlocked = false;
-
-  tabs = this.args.collections.map((collection, index) => {
-    return {
-      name: collection.name,
-      index,
-    };
-  });
-
-  @action setActiveCollection(index) {
-    this.activeCollection = this.args.collections[index];
-  }
 
   @action refreshCollections() {
     if (!this.isRefreshBlocked) {
