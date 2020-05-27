80e2679f3f04f7f36a7fee5d0626853a8ddc8241
Pjaerr <joshcharlesjackson@gmail.com>
2020-05-27 16:55:15 +0100

    working on the menu - still WIP

--- frontend/app/components/menu.js
+++ frontend/app/components/menu.js
@@ -1,6 +1,21 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 
 export default class MenuComponent extends Component {
   @service collectionManager;
+  @tracked isMenuOpen = false;
+
+  @action toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+
+    if (this.isMenuOpen) {
+      document.documentElement.classList.add("menu-open");
+      document.body.classList.add("menu-open");
+    } else {
+      document.documentElement.classList.remove("menu-open");
+      document.body.classList.remove("menu-open");
+    }
+  }
 }
