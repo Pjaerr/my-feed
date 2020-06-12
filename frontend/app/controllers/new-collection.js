9415f9c6654d80309c7097faf9954bcd47d50596
Pjaerr <joshcharlesjackson@gmail.com>
2020-06-12 11:50:39 +0100

    Implemented basic new-collection page

--- frontend/app/controllers/new-collection.js
+++ frontend/app/controllers/new-collection.js
@@ -1,89 +1,39 @@
 import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
 
-export default class BlogPostController extends Controller {
+export default class NewCollectionController extends Controller {
   @service collectionManager;
 
-  //This is temporary. The start new collection will eventually just be a <LinkTo> the relevant route
-  @action startNewCollection() {
-    this.collectionManager.collections = [
-      {
-        name: "Frontend",
-        feeds: [
-          "https://joshuaj.co.uk/rss.xml",
-          "https://joshwcomeau.com/rss.xml",
-        ],
-        items: [
-          {
-            title: "Building a Desktop App using Svelte and Electron",
-            image:
-              "https://joshuaj.co.uk/building-desktop-app-svelte-electron/cover_image.jpg",
-            description:
-              "In this blogpost I show you how easy it is to build a basic markdown editor for desktop using Svelte and Electron.",
-            url:
-              "https://joshuaj.co.uk/blog/building-desktop-app-svelte-electron",
-            date: "Sat, 22 Feb 2020",
-          },
-          {
-            title: "Lessons Learned Speaking at Conferences",
-            image:
-              "https://joshwcomeau.com/images/og-lessons-learned-speaking-at-conferences.png",
-            description:
-              "This post was originally published on Medium in August 2018. I've rewritten it to include stuff I've learned since, and discuss how the COVID-19 pandemic affects it all.",
-            url:
-              "https://joshwcomeau.com/blog/lessons-learned-speaking-at-conferences",
-            date: "Fri, 6 May 2020",
-          },
-          {
-            title: "Lets Create: A Data Visualization using Svelte",
-            image:
-              "https://joshuaj.co.uk/building-desktop-app-svelte-electron/cover_image.jpg",
-            description:
-              "In this blogpost I show you how easy it is to build a basic markdown editor for desktop using Svelte and Electron.",
-            url:
-              "https://joshuaj.co.uk/blog/building-desktop-app-svelte-electron",
-            date: "Sat, 22 Feb 2020",
-          },
-        ],
-      },
-      {
-        name: "Design",
-        feeds: [
-          "https://joshuaj.co.uk/rss.xml",
-          "https://joshwcomeau.com/rss.xml",
-        ],
-        items: [
-          {
-            title: "Test Item",
-            image:
-              "https://joshuaj.co.uk/building-desktop-app-svelte-electron/cover_image.jpg",
-            description:
-              "In this blogpost I show you how easy it is to build a basic markdown editor for desktop using Svelte and Electron.",
-            url: "https://joshuaj.co.uk/",
-            date: "Sat, 22 Feb 2020",
-          },
-        ],
-      },
-      {
-        name: "Career",
-        feeds: [
-          "https://joshuaj.co.uk/rss.xml",
-          "https://joshwcomeau.com/rss.xml",
-        ],
-        items: [
-          {
-            title: "Test Item",
-            image:
-              "https://joshuaj.co.uk/building-desktop-app-svelte-electron/cover_image.jpg",
-            description:
-              "In this blogpost I show you how easy it is to build a basic markdown editor for desktop using Svelte and Electron.",
-            url: "https://joshuaj.co.uk/",
-            date: "Sat, 22 Feb 2020",
-          },
-        ],
-      },
-    ];
+  @tracked name = "New Collection";
+  @tracked currentFeed = "";
+  @tracked feeds = [];
+
+  @tracked isLoading = false;
+
+  @action addFeed() {
+    //Could probably do some standard checking to see if this is a valid URL to catch easy stuff
+    //Also check that the feed doesn't already exist within the feeds so we don't have duplicates
+    //?Could also limit the number of feeds per collection in the MVP
+    this.feeds = [...this.feeds, this.currentFeed];
+
+    this.currentFeed = "";
+  }
+
+  @action removeFeed(feedToRemove) {
+    this.feeds = this.feeds.filter((feed) => feed !== feedToRemove);
+  }
+
+  @action async createNewCollection() {
+    this.isLoading = true;
+
+    await this.collectionManager.createNewCollection({
+      name: this.name,
+      feeds: this.feeds,
+    });
+
+    this.isLoading = false;
 
     this.transitionToRoute("/");
   }
